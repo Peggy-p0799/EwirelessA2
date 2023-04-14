8498f5ebddedc0ff1aace2658cedce985baf2de5
PP <115942542+Peggy-p0799@users.noreply.github.com>
2023-04-14 14:53:01 +0100

    UI2nd

--- app/src/main/java/com/example/a2_integration/task2pdr.java
+++ app/src/main/java/com/example/a2_integration/task2pdr.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class task2pdr extends Fragment {
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
     }
 
@@ -22,6 +24,18 @@ public class task2pdr extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_task2pdr, container, false);
+
+        Spinner spinner_building = (Spinner) view.findViewById(R.id.spnBuilding);
+        Spinner spinner_floor = (Spinner) view.findViewById(R.id.spnFloor);
+
+        ArrayAdapter<CharSequence> buildingAdapter = ArrayAdapter.createFromResource(getContext(),R.array.Buildings,R.layout.layout_spinner_text);
+        ArrayAdapter<CharSequence> floorAdapter = ArrayAdapter.createFromResource(getContext(),R.array.Floors_N,R.layout.layout_spinner_text);
+
+        buildingAdapter.setDropDownViewResource(R.layout.layout_spinner_text);
+        floorAdapter.setDropDownViewResource(R.layout.layout_spinner_text);
+
+        spinner_building.setAdapter(buildingAdapter);
+        spinner_floor.setAdapter(floorAdapter);
 
         return view;
     }
